e0dd56f75c53ad89c5a0958610f71388fde22a98
ramyabyyu <bintantramy@gmail.com>
2022-12-07 13:52:57 +0700

    setup tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h3 className="text-xl text-fuchsia-600">Hello World</h3>
+    </div>
+  );
+};
+
+export default App;
